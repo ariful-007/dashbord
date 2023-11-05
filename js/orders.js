0e25799c55755d8f7a9fffcf8236595a6bbab1ff
ariful007 <arifulislamwd007@gmail.com>
2023-11-05 17:00:34 +0600

    first commit

---
+++ js/orders.js
@@ -0,0 +1,50 @@
+const Orders =[
+    {
+        productName:"Foldable Mini Drone",
+        productNumber:"85631",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+
+    {
+        productName:"LARVENDER KE102 Drone",
+        productNumber:"36378",
+        paymentStatus:"Refunded",
+        shipping:"Declined"
+    },
+
+    {
+        productName:"Ruko F11 Pro Drone",
+        productNumber:"49347",
+        paymentStatus:"Due",
+        Shipping:"Pending"
+    },
+
+    {
+        productNmae:"Drone With Camera drone",
+        productNumber:"96996",
+        paymentStatus:"Paid",
+        Shipping:"Delivered"
+    },
+
+    {
+        productName:"GPS 4k Drone",
+        productNumber:"22821",
+        paymentStatus:"Paid",
+        shipping:"Delivered"
+    },
+
+    {
+        productNmae:"DJI Ari 2S",
+        productNumber:"81475",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+
+    {
+        productName:"Lozenge Drone",
+        productNumber:"00482",
+        paymentStatus:"Paid",
+        shipping:"Delivered"
+    },
+]
